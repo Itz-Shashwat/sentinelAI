85b1d587b7bc09fd5acc4414452c3e5fbede12ba
Shashwat Sharma <shashwatsharmammiii@gmail.com>
2024-12-26 16:59:06 +0530

    syntheticapi code updated for render deployment with 500 and 0.0.0.0 port configuration

--- backend/gans_training/generate synthetic/generatesyntheticAPIcalling.py
+++ backend/gans_training/generate synthetic/generatesyntheticAPIcalling.py
@@ -67,4 +67,5 @@ def generate_data():
         return {'error': 'An error occurred while generating the data'}, 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
